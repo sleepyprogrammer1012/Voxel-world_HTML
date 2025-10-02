65baf2cc034acf79393391c0706a05f413b6c0f3
Sleepy Gamer <bendtomywillgamers@gmail.com>
2025-10-02 16:46:56 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@
         scene.add(directionalLight);
         
         const sunGeometry = new THREE.SphereGeometry(20, 32, 32);
-        const sunMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00, emissive: 0xffff00 });
+        const sunMaterial = new THREE.MeshLambertMaterial({ color: 0xffff00, emissive: 0xffff00 });
         const sun = new THREE.Mesh(sunGeometry, sunMaterial);
         scene.add(sun);
 
