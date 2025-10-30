c05dc25379bcb99c115b14ef2983a5f7b391e277
sleepyprogrammer1012 <bendtomywillgamers@gmail.com>
2025-10-30 11:57:53 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -236,6 +236,9 @@
           const { type, chunkX, chunkZ, blocks } = e.data;
           if (type === "chunkGenerated") {
             Object.entries(blocks).forEach(([key, value]) => world.set(key, value));
+              const [x, y, z] = key.split(',').map(Number);   // parse worker’s string key
+              world.set(getBlockKey(x, y, z), value);
+            });
             generateChunkMesh(chunkX, chunkZ);
           }
         };
