e5d4fc9be360e0882bcfcd67a37fb8e05ffd7c3d
sleepyprogrammer1012 <bendtomywillgamers@gmail.com>
2025-10-30 12:45:04 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -708,7 +708,6 @@
         window.addEventListener('resize', () => { camera.aspect = window.innerWidth / window.innerHeight; camera.updateProjectionMatrix(); renderer.setSize(window.innerWidth, window.innerHeight); });
 
         // Start
-        updateChunks();
         animate();
       
 
